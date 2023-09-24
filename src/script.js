7d72459d06ae3f0efea4f90bab99b3fdb0269f31
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-09-24 09:23:47 +0300

    goit

--- src/script.js
+++ src/script.js
@@ -1,97 +1,103 @@
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import InfiniteScroll from 'infinite-scroll';
 import { fetchImages, PER_PAGE, API_KEY, BASE_URL } from './js/api';
 import createMarkup from './js/template/markup';
 import refs from './js/refs';
 
-refs.formEl.addEventListener('submit', onSubmitSearch);
 
-let currentPage = 1;
-let value = '';
-let totalHitsImg = 0;
+document.addEventListener('DOMContentLoaded', () => {
+  refs.formEl.addEventListener('submit', onSubmitSearch);
 
-function onload() {
-  currentPage += 1;
-  getImage();
-}
+  let currentPage = 1;
+  let value = '';
+  let totalHitsImg = 0;
 
-function onSubmitSearch(e) {
-  e.preventDefault();
-  value = e.currentTarget.elements.searchQuery.value.trim();
-  if (!value) {
-    message('Please write correct data!');
-    return;
-  }
+  let lightbox;
 
-  clearGallery();
-  getImage();
-}
-
-async function getImage() {
-  try {
-    const resp = await fetchImages(currentPage, value);
-    refs.galleryWrapperEl.insertAdjacentHTML(
-      'beforeend',
-      createMarkup(resp.hits)
-    );
-    
-    lightbox.refresh();
-
-   
-    if (resp.total === 0) {
+  function onSubmitSearch(e) {
+    e.preventDefault();
+    value = e.currentTarget.elements.searchQuery.value.trim();
+    if (!value) {
       message('Please write correct data!');
       return;
     }
-    totalHitsImg += resp.hits.length;
 
-    if (totalHitsImg === resp.totalHits || totalHitsImg < 40) {
-      refs.spanEl.textContent =
-        'Were sorry, but you ve reached the end of search results.';
-      return;
-    }
-    if (totalHitsImg > 40) {
-      const { height: cardHeight } =
-        refs.galleryWrapperEl.firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-      });
-    }
-  } catch (error) {
-    Report.failure('404', '');
-    console.error(error);
+    clearGallery();
+    getImage();
   }
-}
 
+  async function getImage() {
+    try {
+      const resp = await fetchImages(currentPage, value);
+      refs.galleryWrapperEl.insertAdjacentHTML(
+        'beforeend',
+        createMarkup(resp.hits)
+      );
+      
+      if (!lightbox) {
+        lightbox = new SimpleLightbox('.gallery a', {
+          captions: true,
+          captionsData: 'alt',
+          captionPosition: 'bottom',
+          captionDelay: 250,
+        });
+      } else {
+        lightbox.refresh();
+      }
 
-const infiniteScroll = new InfiniteScroll(refs.galleryWrapperEl, {
-  responseType: 'json',
-  history: false,
-  status: '.scroll-status',
+      if (resp.total === 0) {
+        message('Please write correct data!');
+        return;
+      }
+      totalHitsImg += resp.hits.length;
+
+      if (totalHitsImg === resp.totalHits || totalHitsImg < 40) {
+        refs.spanEl.textContent =
+          "We're sorry, but you've reached the end of search results.";
+        return;
+      }
+      if (totalHitsImg > 40) {
+        const { height: cardHeight } =
+          refs.galleryWrapperEl.firstElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+          top: cardHeight * 2,
+          behavior: 'smooth',
+        });
+      }
+    } catch (error) {
+      Report.failure('404', '');
+      console.error(error);
+    }
+  }
+
+  function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        getImage();
+      }
+    });
+  }
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  };
+
+  const intersectionObserver = new IntersectionObserver(handleIntersection, options);
+  
+  intersectionObserver.observe(refs.bottomElement);
+
+  function message(sms) {
+    Report.warning('Warning!', `${sms}`);
+  }
+
+  function clearGallery() {
+    totalHitsImg = 0;
+    currentPage = 1;
+    refs.spanEl.innerHTML = '';
+    refs.galleryWrapperEl.innerHTML = '';
+  }
 });
-
-infiniteScroll.on('load', onload);
-
-infiniteScroll.on('error', () => {
-  Report.failure('404', '');
-});
-
-function message(sms) {
-  Report.warning(`Warning!`, `${sms}`);
-}
-let lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
-
-function clearGallery() {
-  totalHitsImg = 0;
-  currentPage = 1;
-  refs.spanEl.innerHTML = '';
-  refs.galleryWrapperEl.innerHTML = '';
-}
